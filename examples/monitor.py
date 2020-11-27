d536e57dc5fd274bb2cfcba858941f32de5a5fd8
emher <emil.h.eriksen@gmail.com>
2020-11-27 09:57:29 +0100

    Added Monitor example

---
+++ examples/monitor.py
@@ -0,0 +1,32 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash import Dash, no_update
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+from dash_extensions import Monitor
+
+app = Dash()
+app.layout = html.Div(Monitor([
+    dcc.Input(id="deg-fahrenheit", autoComplete="off", type="number"),
+    dcc.Input(id="deg-celsius", autoComplete="off", type="number")],
+    probes=dict(deg=[dict(id="deg-fahrenheit", prop="value"), dict(id="deg-celsius", prop="value")]), id="monitor")
+)
+
+
+@app.callback([Output("deg-fahrenheit", "value"), Output("deg-celsius", "value")], [Input("monitor", "data")])
+def sync_inputs(data):
+    # Get value and trigger id from monitor.
+    try:
+        probe = data["deg"]
+        trigger_id, value = probe["trigger"]["id"], float(probe["value"])
+    except (TypeError, KeyError):
+        raise PreventUpdate
+    # Do the appropriate update.
+    if trigger_id == "deg-fahrenheit":
+        return no_update, (value - 32) * 5 / 9
+    elif trigger_id == "deg-celsius":
+        return value * 9 / 5 + 32, no_update
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
