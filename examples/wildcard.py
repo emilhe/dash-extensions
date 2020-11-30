1b8e6694dd9a6a5508a0ede8b873d15fbe4758ec
emher <emil.h.eriksen@gmail.com>
2020-11-30 10:15:35 +0100

    Wild card grouping now seems to be working with MATCH and ALL

--- examples/wildcard.py
+++ examples/wildcard.py
@@ -3,25 +3,33 @@ from flask import Flask
 import dash_html_components as html
 
 from dash.dependencies import ALL, MATCH
-from dash_extensions.enrich import Trigger, Input, Output
+from dash_extensions.enrich import Input, Output
 
 fapp = Flask(__name__)
 app = Dash(__name__, server=fapp)
 app.suppress_callback_exceptions = True
 
 
-@app.callback(
-    Output("out", "children"), Input(dict(type="delete", id=ALL), "n_clicks"), group="group"
+@app.callback(Output("all", "children"),
+              Input(dict(type="btn", id=ALL), "n_clicks"), group="A")
+def all(args):
+    print("ALL")
+    return args
+
+
+@app.callback(Output(dict(type="MATCH", id=MATCH), "children"),
+              Input(dict(type="btn", id=MATCH), "n_clicks"), group="C")
+def match(args):
+    print("MATCH")
+    return args
+
+
+ids = [1, 2, 3]
+app.layout = html.Div(
+    [html.Button(f"Button{id}", id=dict(type="btn", id=id)) for id in ids] +
+    [html.Div(children="ALL"), html.Div(id="all", children="all")] +
+    [html.Div(children="MATCH")] +
+    [html.Div(id=dict(type="MATCH", id=id)) for id in ids]
 )
-def do(d):
-    print("Triggered")
-    return d
 
-
-app.layout = html.Div([
-    html.Button("B1", id=dict(type="delete", id="1")),
-    html.Button("B2", id=dict(type="delete", id="2")),
-    html.Div(id="out")
-])
-
-app.run_server(debug=False)
+app.run_server()
