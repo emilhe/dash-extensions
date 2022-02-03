4668c62558a700eb053d392680ba274b11b2ba7c
Snehil Vijay <snehilvj@outlook.com>
2022-02-03 23:41:08 +0530

    Renamed the component and add readyState prop

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,17 +1,17 @@
 /* eslint-disable import/prefer-default-export */
-import Download from './components/Download.react';
-import Lottie from './components/Lottie.react';
-import Keyboard from './components/Keyboard.react';
-import Monitor from './components/Monitor.react';
-import Burger from './components/Burger.react';
-import Ticker from './components/Ticker.react';
-import WebSocket from './components/WebSocket.react';
-import BeforeAfter from './components/BeforeAfter.react';
-import Mermaid from './components/Mermaid.react';
-import DeferScript from './components/DeferScript.react';
-import Purify from './components/Purify.react';
-import EventListener from './components/EventListener.react';
-import ServerSentEvents from './components/ServerSentEvents.react';
+import Download from "./components/Download.react";
+import Lottie from "./components/Lottie.react";
+import Keyboard from "./components/Keyboard.react";
+import Monitor from "./components/Monitor.react";
+import Burger from "./components/Burger.react";
+import Ticker from "./components/Ticker.react";
+import WebSocket from "./components/WebSocket.react";
+import BeforeAfter from "./components/BeforeAfter.react";
+import Mermaid from "./components/Mermaid.react";
+import DeferScript from "./components/DeferScript.react";
+import Purify from "./components/Purify.react";
+import EventListener from "./components/EventListener.react";
+import DashEventSource from "./components/DashEventSource.react";
 
 export {
     Download,
@@ -26,5 +26,5 @@ export {
     DeferScript,
     Purify,
     EventListener,
-    ServerSentEvents
+    DashEventSource,
 };
