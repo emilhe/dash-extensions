99b6ad86477d07990a7b3ea32db6f49df1adf17c
emher <emher@vestas.com>
2022-02-03 22:08:36 +0100

    Fix example

--- examples/eventsource_client.py
+++ examples/eventsource_client.py
@@ -1,11 +1,11 @@
 from dash import Dash, html, Input, Output, dcc
-from dash_extensions import WebSocket
+from dash_extensions import EventSource
 
 # The url of server that emits the events.
 sse_url = "http://127.0.0.1:8000"
 # Create small example app.
 app = Dash(__name__)
-app.layout = html.Div([dcc.Graph(id="graph"), DashEventSource(id="sse", url=sse_url)])
+app.layout = html.Div([dcc.Graph(id="graph"), EventSource(id="sse", url=sse_url)])
 # You could also use a normal callback, but client side callbacks yield better performance.
 app.clientside_callback(
     """
