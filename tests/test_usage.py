96ac2d26488bcbdd0a7f73c38f4e2f3599fc4732
emher <emil.h.eriksen@gmail.com>
2022-02-18 18:18:30 +0100

    Debug action

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,25 +1,32 @@
-from dash.testing.application_runners import import_app
+import os
+from dash import Dash, Input, Output, dcc, html
+
+from dash.testing.wait import until
 
 
-# Basic test for the component rendering.
-# The dash_duo pytest fixture is installed with dash (v1.0+)
-def test_render_component(dash_duo):
-    # Start a dash app contained as the variable `app` in `usage.py`
-    app = import_app('usage')
-    dash_duo.start_server(app)
+def test_dltx001_download_text(dash_dcc):
+    text = "Hello, world!"
+    filename = "hello.txt"
+    # Create app.
+    app = Dash(__name__, prevent_initial_callbacks=True)
+    app.layout = html.Div([html.Button("Click", id="btn"), dcc.Download(id="download")])
 
-    # Get the generated component input with selenium
-    # The html input will be a children of the #input dash component
-    my_component = dash_duo.find_element('#input > input')
+    @app.callback(Output("download", "data"), Input("btn", "n_clicks"))
+    def download(_):
+        return dcc.send_string(text, filename)
 
-    assert 'my-value' == my_component.get_attribute('value')
+    dash_dcc.start_server(app)
 
-    # Clear the input
-    dash_duo.clear_input(my_component)
+    # Check that there is nothing before clicking
+    fp = os.path.join(dash_dcc.download_path, filename)
+    assert not os.path.isfile(fp)
 
-    # Send keys to the custom input.
-    my_component.send_keys('Hello dash')
+    dash_dcc.find_element("#btn").click()
 
-    # Wait for the text to equal, if after the timeout (default 10 seconds)
-    # the text is not equal it will fail the test.
-    dash_duo.wait_for_text_to_equal('#output', 'You have entered Hello dash')
+    # Check that a file has been download, and that it's content matches the original text.
+    until(lambda: os.path.exists(fp), 10)
+    with open(fp, "r") as f:
+        content = f.read()
+    assert content == text
+
+    assert dash_dcc.get_logs() == []
