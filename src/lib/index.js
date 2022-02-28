1f11ca489430594c0e37350f32f2ee63a5582adf
emher <emil.h.eriksen@gmail.com>
2022-02-28 22:37:47 +0100

    Dropping Download and Monitor

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable import/prefer-default-export */
-import Download from "./components/Download.react";
 import Lottie from "./components/Lottie.react";
 import Keyboard from "./components/Keyboard.react";
-import Monitor from "./components/Monitor.react";
 import Burger from "./components/Burger.react";
 import Ticker from "./components/Ticker.react";
 import WebSocket from "./components/WebSocket.react";
@@ -14,10 +12,8 @@ import EventListener from "./components/EventListener.react";
 import EventSource from "./components/EventSource.react";
 
 export {
-    Download,
     Lottie,
     Keyboard,
-    Monitor,
     Burger,
     WebSocket,
     Ticker,
