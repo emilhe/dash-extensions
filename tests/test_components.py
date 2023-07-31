a9fdba93c9cb2e8fd486eec19871f943671b8a71
Emil Haldrup Eriksen <emil.h.eriksen@gmail.com>
2023-07-31 12:22:47 +0200

    Drop Ticker test (for now)

--- tests/test_components.py
+++ tests/test_components.py
@@ -2,11 +2,11 @@ import pytest
 from dash.testing.application_runners import import_app
 
 # All custom dash components.
-components = ["before_after", "defer_script", "event_listener", "lottie", "mermaid", "purify", "ticker"]
+components = ["before_after", "defer_script", "event_listener", "lottie", "mermaid", "purify"]
 
 
 # Basic test for the component rendering.
 @pytest.mark.parametrize("component", components)
 def test_render_components(dash_duo, component):
-    app = import_app(f"tests.components.{component}")
+    app = import_app(f"tests.components.{component}"), "ticker"
     dash_duo.start_server(app)
