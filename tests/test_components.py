b04a5a120fdc11a221963140b3dd2e3000ce3e27
Emil Haldrup Eriksen <emil.h.eriksen@gmail.com>
2023-07-31 12:54:27 +0200

    Revert bug

--- tests/test_components.py
+++ tests/test_components.py
@@ -8,5 +8,5 @@ components = ["before_after", "defer_script", "event_listener", "lottie", "merma
 # Basic test for the component rendering.
 @pytest.mark.parametrize("component", components)
 def test_render_components(dash_duo, component):
-    app = import_app(f"tests.components.{component}"), "ticker"
+    app = import_app(f"tests.components.{component}")
     dash_duo.start_server(app)
