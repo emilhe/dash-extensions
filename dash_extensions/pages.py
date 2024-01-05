8c4a7fbd563688e502b7e53b1d841ee9098d6d4d
emilhe <emil.h.eriksen@gmail.com>
2024-01-05 15:49:07 +0100

    First take at dynamic props

--- dash_extensions/pages.py
+++ dash_extensions/pages.py
@@ -1,7 +1,8 @@
+import json
 import dash
 from collections import OrderedDict
-from typing import Optional
-from dash import html, Input, Output, clientside_callback
+from typing import Optional, Any
+from dash import html, Input, Output, State, clientside_callback
 from dash.development.base_component import Component
 
 """
@@ -9,7 +10,8 @@ This module holds utilities related to the [Dash pages](https://dash.plotly.com/
 """
 
 _ID_CONTENT = "_component_content"
-_PATH_REGISTRY = OrderedDict()
+_COMPONENT_PATH_REGISTRY = OrderedDict()
+_PROP_PATH_REGISTRY = OrderedDict()
 _CONTAINER_REGISTRY = {}
 _COMPONENT_CONTAINER = html.Div(id=_ID_CONTENT, disable_n_clicks=True)
 
@@ -18,12 +20,18 @@ _COMPONENT_CONTAINER = html.Div(id=_ID_CONTENT, disable_n_clicks=True)
 _original_register_page = dash.register_page
 
 
-def _register_page(*args, dynamic_components=None, **kwargs):
+def _register_page(*args, dynamic_components=None, dynamic_props=None, **kwargs):
     _original_register_page(*args, **kwargs)
-    if dynamic_components is None:
-        return
+    # Resolve page.
     module = kwargs['module'] if 'module' in kwargs else args[0]
     page = dash.page_registry[module]
+    # Register callbacks for dynamic props.
+    if dynamic_props is not None:
+        for component in dynamic_props:
+            set_props(component, page['path'], dynamic_props[component])
+    # Resolve any dynamic components.
+    if dynamic_components is None:
+        return
     for component in dynamic_components:
         set_visible(component, page['path'])
 
@@ -34,6 +42,16 @@ dash.register_page = _register_page
 # endregion
 
 # region Public interface
+
+def set_default_container(container: Component):
+    """
+    Per default, dynamic components are rendered into the '_COMPONENT_CONTAINER' declared above.
+    Use this function to change the default container.
+    :param container: the container into which components will be rendered by default
+    :return: None
+    """
+    _COMPONENT_CONTAINER = container
+
 
 def assign_container(component: Component, container: Component):
     """
@@ -55,7 +73,19 @@ def set_visible(component: Component, path: str):
     :param path: the (url) path for which the component should be visible
     :return: None
     """
-    _PATH_REGISTRY.setdefault(component, []).append(path)
+    _COMPONENT_PATH_REGISTRY.setdefault(component, []).append(path)
+
+
+def set_props(component: Component, path: str, prop_map: dict[str, Any]):
+    """
+    Register path(s) for which a particular props should be set.
+    :param component: the (dynamic) component in question
+    :param path: the (url) path for which the props should be set
+    :param prop_map: the props, i.e. (prop name, prop value) pairs
+    :return: None
+    """
+    for prop in prop_map:
+        _PROP_PATH_REGISTRY.setdefault(component, OrderedDict()).setdefault(prop, {})[path] = prop_map[prop]
 
 
 def setup_dynamic_components() -> html.Div:
@@ -85,7 +115,8 @@ def _prepare_container(container: Optional[Component] = None):
 def _setup_callbacks():
     store = dash.dash._ID_STORE
     location = dash.dash._ID_LOCATION
-    components = list(_PATH_REGISTRY.keys())
+    # Setup callbacks for dynamic components.
+    components = list(_COMPONENT_PATH_REGISTRY.keys())
     for component in components:
         # Wrap in div to ensure 'hidden' prop exists.
         wrapper = html.Div(component, disable_n_clicks=True, hidden=True)
@@ -93,9 +124,35 @@ def _setup_callbacks():
         container = _prepare_container(_CONTAINER_REGISTRY.get(component, _COMPONENT_CONTAINER))
         container.children.append(wrapper)
         # Setup callback.
-        f = f"function(y, x){{const paths = {_PATH_REGISTRY[component]}; return !paths.includes(x);}}"
-        clientside_callback(f, Output(wrapper, "hidden"),
+        f = f"""function(y, x){{
+            const paths = {_COMPONENT_PATH_REGISTRY[component]}; 
+            return !paths.includes(x);
+        }}"""
+        clientside_callback(f, Output(wrapper, "hidden", allow_duplicate=True),
                             Input(store, "data"),
-                            State(location, "pathname"))
+                            State(location, "pathname"),
+                            prevent_initial_call='initial_duplicate')
+    # Setup callbacks for dynamic props.
+    components = list(_PROP_PATH_REGISTRY.keys())
+    for component in components:
+        for prop in _PROP_PATH_REGISTRY[component]:
+            path_map = _PROP_PATH_REGISTRY[component][prop]
+            default = getattr(component, prop, None)
+            # Setup callback.
+            f = f"""function(y, x){{
+                const path_map = JSON.parse(\'{json.dumps(path_map)}\'); 
+                console.log(path_map);
+                console.log(x);
+                if (x in path_map){{
+                    console.log("RETURN X");
+                    return path_map[x];
+                }}
+                console.log("RETURN DEFAULT");
+                return JSON.parse(\'{json.dumps(default)}\'); 
+            }}"""
+            clientside_callback(f, Output(component, prop, allow_duplicate=True),
+                                Input(store, "data"),
+                                State(location, "pathname"),
+                                prevent_initial_call='initial_duplicate')
 
 # endregion
