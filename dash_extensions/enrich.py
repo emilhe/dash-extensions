c57f06e60cdef62736d19d25b9febafaef9a129e
Fran Hrženjak <fran.hrzenjak@gmail.com>
2024-03-27 23:54:30 +0100

    wrap callbacks with triggers and filter them out

--- dash_extensions/enrich.py
+++ dash_extensions/enrich.py
@@ -11,6 +11,8 @@ import struct
 import sys
 import threading
 import uuid
+from itertools import compress
+
 import plotly
 import dash
 
@@ -952,8 +954,6 @@ class Trigger(Input):
 
 class TriggerTransform(DashTransform):
 
-    # NOTE: This transform cannot be implemented for clientside callbacks since the JS can't be modified from here.
-
     def apply_serverside(self, callbacks):
         for callback in callbacks:
             is_trigger = [isinstance(item, Trigger) for item in callback.inputs]
@@ -963,6 +963,24 @@ class TriggerTransform(DashTransform):
             # If so, filter the callback args.
             f = callback.f
             callback.f = filter_args(is_trigger)(f)
+        return callbacks
+
+    def apply_clientside(self, callbacks):
+        for callback in callbacks:
+            is_not_trigger = [not isinstance(item, Trigger) for item in callback.inputs]
+            # Check if any triggers are there.
+            if all(is_not_trigger):
+                continue
+            # If so, filter the callback args.
+            args = [f"arg{i}" for i in range(len(callback.inputs))]
+            filtered_args = compress(args, is_not_trigger)
+            if isinstance(callback.f, ClientsideFunction):
+                callback.f = f"window.dash_clientside['{callback.f.namespace}']['{callback.f.function_name}']"
+            callback.f = f"""
+function({", ".join(args)}) {{
+const func = {callback.f};
+return func({", ".join(filtered_args)});
+}}"""
         return callbacks
 
 
