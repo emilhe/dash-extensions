963fc131d18ae4da0ada2f5be5d18bfdc3209e98
emilhe <emil.h.eriksen@gmail.com>
2024-11-27 21:46:19 +0100

    Add get_event_dependency

--- dash_extensions/events.py
+++ dash_extensions/events.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Type, TypeVar
 
-from dash import Input, dcc, set_props
+from dash import set_props
+from dash_extensions.enrich import Input, dcc
+from dash.dependencies import DashDependency
+
+T = TypeVar("T", bound=DashDependency)
 
 
 def dispatch_event(event: str, payload: Optional[dict] = None):
@@ -20,12 +24,19 @@ def dispatch_event(event: str, payload: Optional[dict] = None):
     )
 
 
+def get_event_dependency(event: str, dependency_type: Type[T]) -> T:
+    """
+    Get DashDependency (Input, Output, State) for a particular event type.
+    """
+    _event_registry.add(event)
+    return dependency_type(_get_event_id(event), "data")
+
+
 def add_event_listener(event: str) -> Input:
     """
     Listen for a particular event type.
     """
-    _event_registry.add(event)
-    return Input(_get_event_id(event), "data")
+    return get_event_dependency(event, Input)
 
 
 def resolve_event_components() -> list[dcc.Store]:
